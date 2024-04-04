e92ec193143b51891d4dfbfd72a3796f7b34527c
DigAguiar <diogobarreto2004@gmail.com>
2024-04-04 20:06:03 -0300

    footer corrigido parcialmente / lista dinamica de monges em prod

--- src/app/Types/Monge.ts
+++ src/app/Types/Monge.ts
@@ -1,17 +1,23 @@
 export interface IMonge {
     id : number;
     nome : string;
-
+    idade : number;
 
 
 }
 
 export const staticListMongesTS : IMonge[] = [
-    { id: 1, nome: "Monge 1" },
-    { id: 2, nome: "Monge 2" },
-    { id: 3, nome: "Monge 3" },
-    { id: 4, nome: "Monge 4" }
-
+    { id: 1, nome: "Monge 1", idade: 20 },
+    { id: 2, nome: "Monge 2", idade : 30 },
+    { id: 3, nome: "Monge 3", idade : 40 },
+    { id: 4, nome: "Monge 4", idade : 50 },
+    { id: 5, nome: "Monge 5", idade : 60 },
+    { id: 6, nome: "Monge 6", idade : 52 },
+    { id: 7, nome: "Monge 7", idade : 23 },
+    { id: 8, nome: "Monge 8", idade : 47 },
+    { id: 9, nome: "Monge 9", idade : 12 },
+    { id: 10, nome: "Monge 10", idade : 9 },
+        
 ]
 
 
--- src/app/monges/monges.component.ts
+++ src/app/monges/monges.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListaMongeService } from '../services/lista-monge.service';
+import { IMonge } from '../Types/Monge';
 
 @Component({
   selector: 'app-monges',
   templateUrl: './monges.component.html',
   styleUrl: './monges.component.css'
 })
-export class MongesComponent {
+export class MongesComponent implements OnInit{
+
+  listaMonges! : IMonge[];
+  constructor(
+    private listService: ListaMongeService
+  ) { }
+
+  ngOnInit(): void {
+    this.listaMonges = this.listService.getAllMonges();
+  }
+
+
+
 
 }
